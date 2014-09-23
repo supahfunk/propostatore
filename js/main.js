64afb3198d8da2d920aaf63f6f5043565557a584
Fabio <fabio.ottaviani@gmail.com>
2014-09-24 00:25:04 +0200

    tutto canvas

--- js/main.js
+++ js/main.js
@@ -93,50 +93,80 @@ function closeZoom() {
 
 var Imgs = [];
 var Srcs = [];
+var Mins = [];
 var Ctx = [];
+var Mtx = [];
 
-$('.thumb img').each(function (i) {
-    $(this).wrap('<div class="t' + i +'"></div>').remove();
-    Srcs.push($(this).attr('src'));
-    $('.t' + i).html('<canvas id="c' + i + '" width="120" height="120"></canvas>');
-    Ctx[i] = document.getElementById('c' + i).getContext('2d');
-    Imgs[i] = new Image();
-    Imgs[i].src = Srcs[i];
-    Imgs[i].onload = function () {
-        w = this.width;
-        h = this.height;
-        console.log(w);
-        Ctx[i].drawImage(Imgs[i], 0, 0, 120, 120*h/w);
-    }
-});
-
-$('canvas').each(function(i){
-    $(this).click(function () {
-        $zoom.html('<canvas id="z"></canvas>');
-        var z = document.getElementById('z');
-        var ctx = z.getContext('2d');
-        var img = new Image();
-        img.src = Srcs[i];
-        img.onload = function () {
-            W = $(window).width();
+function init(){
+    $('.thumb img').each(function (i) {
+        $(this).wrap('<div class="t' + i +'"></div>').remove();
+        Srcs.push($(this).attr('src'));
+        $('.t' + i).html('<canvas id="c' + i + '" width="120" height="120"></canvas>');
+        $('<canvas id="m' + i + '" width="60" height="60"></canvas>').appendTo($miniature);
+        Ctx[i] = document.getElementById('c' + i).getContext('2d');
+        Mtx[i] = document.getElementById('m' + i).getContext('2d');
+        Imgs[i] = new Image();
+        Imgs[i].src = Srcs[i];
+        Imgs[i].onload = function () {
             w = this.width;
             h = this.height;
-            z.width = W;
-            z.height = W * h / w;
-            $('.zoom').show();
-            console.log(w);
-            ctx.drawImage(img, 0, 0, W, W * h / w);
+            Ctx[i].drawImage(Imgs[i], 0, 0, 120, 120*h/w);
+            Mtx[i].drawImage(Imgs[i], 0, 0, 60, 60 * h / w);
         }
     });
-});
+
+    $('.miniature canvas').each(function (i) {
+        console.log(i)
+        $(this).click(function () {
+            $('.active').removeClass('active');
+            $(this).addClass('active');
+            $zoom.html('<canvas id="z"></canvas>');
+            var z = document.getElementById('z');
+            var ctx = z.getContext('2d');
+            var img = new Image();
+            img.src = Srcs[i];
+            img.onload = function () {
+                W = $(window).width();
+                w = this.width;
+                h = this.height;
+                z.width = W;
+                z.height = W * h / w;
+                $('.zoom').fadeIn(time);
+                console.log(w);
+                ctx.drawImage(img, 0, 0, W, W * h / w);
+            }
+        });
+    });
+
+    $('.thumb canvas').each(function (i) {
+        $(this).click(function () {
+            console.log(i);
+            $('.miniature canvas:eq(' + i + ')').trigger('click');
+        });
+    });
+}
+
+function close() {
+    $zoom.hide().html('');
+}
 
 $(document).on('keydown', function (e) {
-    if (e.which == 27) {
-        $zoom.hide().html('');
+    switch (e.which) {
+        case 27:
+            close();
+            break;
+        case 37:
+            $('.active').prev().click();
+            break;
+        case 39:
+            $('.active').next().click();
+            break;
     }
 });
 
-console.log(Imgs);
+$zoom.on('dblclick', function () {
+    close();
+});
 
 
 /*--------------------------------------------------
@@ -147,5 +177,6 @@ $(function () {
     for (i = 1; i <= 10; i++) {
         $('<div class="shaft' + i + '"></div>').appendTo($loading);
     }
+    init();
     // createThumbs();
 });
